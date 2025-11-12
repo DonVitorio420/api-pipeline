a6ad700dea82e38684f93ca26e7cff12cfb32acf
TDonVitorio420 <victor.pinedamo@amigo.edu.co>
2025-11-11 19:47:01 -0500

    feat(posts): CRUD funcional con persistencia en db.json

---
+++ api/db/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, 'db.json');
+
+const readDB = () => {
+  const data = fs.readFileSync(dbPath, 'utf-8');
+  return JSON.parse(data);
+};
+
+const writeDB = (data) => {
+  fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+};
+
+module.exports = { readDB, writeDB };
